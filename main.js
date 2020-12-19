ffa8a2d5d859d6aa86499196b12649ee3af04984
Caroline Eubanks <cmeubanks@gmail.com>
2020-12-19 15:47:41 -0700

    conditional funtion to display side/main/dessert based on selection

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var sideRadio = document.querySelector('.side');
 var mainDishRadio = document.querySelector('.main-dish');
 var dessertRadio = document.querySelector('.dessert');
 var entireMealRadio = document.querySelector('entire-meal');
-var letsCookButton = document.querySelector('lets-cook');
+var letsCookButton = document.querySelector('.lets-cook');
 
 
 /* Event Listeners */
@@ -21,10 +21,28 @@ function grabSide() {
   var selectedSide = sides[mealRandomizer(sides)];
   return selectedSide;
 }
-//mirror grab side for Main dish
-
-//mirror grab side for dessert
-
+function grabMainDish() {
+  var selectedMainDish = mains[mealRandomizer(mains)];
+  return selectedMainDish;
+}
+function grabDessert() {
+  var selectedDessert = desserts[mealRandomizer(desserts)];
+  return selectedDessert;
+}
 //displayMeal function, takes in conditional
 //about above event listeners and displays respective
 //meal type
+//Questions: can I use .value to grab selected user radio button?
+//if a value is grabbed, run true false statements to determine if the radio button
+//function contains a value -try to add a parameter and variable if this doesn't work the first time
+function displayMeal() {
+  if (grabSide() == true) {
+    return selectedSide;
+  } else if (grabMainDish() == true) {
+    return selectedMainDish;
+  } else if (grabDessert() == true) {
+    return selectedDessert;
+  } else {
+    return "You're not hungry?"
+  }
+}
