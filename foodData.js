b91036c33b3c8d8814fe3b1270097a7085dbbcff
Caroline Eubanks <cmeubanks@gmail.com>
2020-12-19 12:23:47 -0700

    added array data and HTML tag

---
+++ foodData.js
@@ -0,0 +1,47 @@
+var sides = [
+  "Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caesar Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"
+];
+var mains = [
+  "Spaghetti and Meatballs",
+  "Pineapple Chicken",
+  "Sushi",
+  "Thai Yellow Curry",
+  "Drunken Noodles",
+  "Chicken Parmesean",
+  "Butternut Squash Soup",
+  "BBQ Chicken Burgers",
+  "Ramen",
+  "Empanadas",
+  "Chicken Fried Rice",
+  "Sheet Pan Fajitas",
+  "Margarita Pizza"
+];
+var desserts = [
+  "Apple Pie",
+  "Lemon Meringue Pie",
+  "Black Forest Cake",
+  "Banana Bread",
+  "Peach Cobbler",
+  "Cheesecake",
+  "Funfetti Cake",
+  "Baklava",
+  "Flan",
+  "Macaroons",
+  "Ice Cream",
+  "Chocolate Cupcakes",
+  "Red Velvet Cake",
+  "Blue Berry Cobbler",
+  "Key Lime Pie",
+  "Cookies",
+  "Chocolate Croissant",
+  "Eclairs"
+];
